9bc6d857f18af72bb95f585ee79f1232cf5ef2d0
e6m9 <alostsound@gmail.com>
2023-11-06 01:32:57 -0500

    partially working, very frustrated

--- assets/script.js
+++ assets/script.js
@@ -17,95 +17,42 @@
 //   {
 //     text = "you got it";
 
-    function genCaps() 
-    {
-      var allCaps = '';
-      var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-      for (var i = 1; i <= 1; i++) {
-        var cap = Math.floor(Math.random() * caps.length + 1);
-
-        allCaps += caps.charAt(cap)
-      }
-      return allCaps;
-    }
 
 
-function genLows()
-{
-  var allLows = '';
-  var lows = 'abcdefghijklmnopqrstuvwxyz';
+    // var caps = genCaps();
+    // var lows = genLows();
+    // var spec = genSpec();
+    // var nums = genNums();
+    // var length = 8;
 
-  for (var i = 1; i <= 1; i++) {
-    var low =Math.floor(Math.random() * lows.length + 1);
+    // function generatePassword()
+    // {
+    //   var genPass = '';
+    //   var varCount = [caps,lows,spec,nums].length;
 
-    allLows += lows.charAt(low)
-  }
-  return allLows;
-  }
+    //   for (let i = 0; i < length; i += varCount) {
+    //     if (caps) {
+    //       genPass += genCaps();
+    //     }
+    //     if (lows) {
+    //       genPass += genLows();
+    //     }
+    //     if (spec) {
+    //       genPass += genSpec();
+    //     }
+    //     if (nums) {
+    //       genPass += genNums();
+    //     }
+    //     var finalPass = genPass.slice(0, length);
+    //     return finalPass;
+    //     }
+    //   }
 
-  function genSpec()
-  {
-    var allSpecs = '';
-    var specs = '!@#$%^&*()_-+=/.>,<":;|][}{';
-
-    for (var i = 1; i <= 1; i++) {
-      var spec = Math.floor(Math.random() * specs.length + 1);
-
-      allSpecs += specs.charAt(spec)
-    }
-    return allSpecs;
-  }
-
-  function genNums()
-  {
-    var allNums = '';
-    var nums = '0123456789';
-
-    for (var i = 1; i <= 1; i++)
-    {
-      var num = Math.floor(Math.random() * nums.length + 1);
-
-      allNums += nums.charAt(num)
-    }
-    return allNums;
-  }
-
-
-    var caps = genCaps();
-    var lows = genLows();
-    var spec = genSpec();
-    var nums = genNums();
-    var length = 8;
-
-    function generatePassword()
-    {
-      var genPass = '';
-      var varCount = [caps,lows,spec,nums].length;
-
-      for (let i = 0; i < length; i += varCount) {
-        if (caps) {
-          genPass += genCaps();
-        }
-        if (lows) {
-          genPass += genLows();
-        }
-        if (spec) {
-          genPass += genSpec();
-        }
-        if (nums) {
-          genPass += genNums();
-        }
-        var finalPass = genPass.slice(0, length);
-        return finalPass;
-        }
-      }
-
-  console.log(genCaps());
-  console.log(genLows());
-  console.log(genSpec());
-  console.log(genNums());
-  console.log(generatePassword());
+  // console.log(genCaps());
+  // console.log(genLows());
+  // console.log(genSpec());
+  // console.log(genNums());
+  // console.log(generatePassword());
 
 
 // function passLength() {
@@ -114,9 +61,7 @@ function genLows()
 
 //randomize 
 
-function testAlert() {
-  alert("you did it!");
-}
+
 
 // Get references to the #generate and copy elements
 var generateBtn = document.querySelector("#generate");
@@ -124,13 +69,7 @@ var generateBtn = document.querySelector("#generate");
 // var copyBtn = document.querySelector("#copy");
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-
-// }
 
 
 
@@ -145,7 +84,124 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-generateBtn.addEventListener("click", function() {
-    alert("you did it!");
+generateBtn.addEventListener("click", function generatePassword() {
+
+  if (confirm("do you want Caps?") == true) {
+    genCaps();
+  } else {
+    allCaps == false;
+  };
+
+  if (confirm("do you want Lows?") == true) {
+    genLows();
+  } else {
+    allLows == false;
+  };
+
+  if (confirm("do you want Special Characters?") == true) {
+    genSpec();
+  } else {
+    allSpec == false;
+  };
+
+  if (confirm("do you want Nums?") == true) {
+    genNums();
+  } else {
+    allNums < 0;
+  };
+
+function genCaps() 
+  {
+    allCaps = '';
+    var caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    for (var i = 1; i <= 2; i++) {
+      var cap = Math.floor(Math.random() * caps.length + 1);
+
+      allCaps += caps.charAt(cap)
+    }
+    return allCaps;
+  }
+
+
+function genLows()
+{
+ allLows = '';
+var lows = 'abcdefghijklmnopqrstuvwxyz';
+
+for (var i = 1; i <= 2; i++) {
+  var low =Math.floor(Math.random() * lows.length + 1);
+
+  allLows += lows.charAt(low)
+}
+return allLows;
+}
+
+function genSpec()
+{
+ allSpec = '';
+  var specs = '!@#$%^&*()-+=/.>,<":;|][}{';
+
+  for (var i = 1; i <= 2; i++) {
+    var spec = Math.floor(Math.random() * specs.length + 1);
+
+    allSpec += specs.charAt(spec)
+  }
+  return allSpec;
+}
+
+function genNums()
+{
+ allNums = '';
+  var nums = '0123456789';
+
+  for (var i = 1; i <= 2; i++)
+  {
+    var num = Math.floor(Math.random() * nums.length + 1);
+
+    allNums += nums.charAt(num)
+  }
+  return allNums;
+}
+
+length = 8;
+
+function generatePassword()
+{
+  var genPass = '';
+  var varCount = [allCaps,allLows,allSpec,allNums].length;
+
+  for (let i = 0; i < length; i += varCount) {
+   if (genCaps()) {
+      genPass += allCaps;
+   }
+   if (genLows()) {
+      genPass += allLows;
+   }
+   if (genSpec()) {
+      genPass += allSpec;
+   }
+   if (genNums()) {
+      genPass += allNums;
+   }
+
+    var finalPass = genPass.slice(0, length);
+    return finalPass;
+    }
+  }
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+  writePassword();
+  console.log(generatePassword());
+
   }
 );
+
+
+//needs to reset to default at start of function
