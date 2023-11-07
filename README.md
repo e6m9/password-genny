cc289f49ac8fdee41ade006bc8e2236bc36572b3
e6m9 <alostsound@gmail.com>
2023-11-06 22:45:36 -0500

    refined code, final edits

--- README.md
+++ README.md
@@ -1,41 +1,15 @@
-# password-genny
-a password generator
+# Password Genny
 
-# 03 JavaScript: Password Generator
+## Description
 
-## Acceptance Criteria
+A random password generator that presents prompts for the password length and which kind of characters to be included in the password.
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-``
+## Usage
 
-to do
- --design genny w/ html and css
-    --create box to hold the following elements
-        ALREADY THERE--add no input text field for password generation
-        ALREADY THERE--add button to generate password
-        DONE--add buttonn to copy password
-        DONE--have fun with colors
-        --consider other fun design choices
+To generate a random password.
 
- --build genny w/ javascript
-    --set up a series of prompts for the following criteria
-        --lowercase
-        --uppercase
-        --numbers
-        --special characters
-        --length of at least 8 characters
+[password-genny-screenshot](./assets/screenshots/Password-Genny.png)
+[password-prompt-screenshot](./assets/screenshots/password-prompt.png)
+[password-generated-screenshot](./assets/screenshots/password-generated.png)
+
+[link-to-site](https://e6m9.github.io/password-genny)
--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,72 @@
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-// writePassword();
-// console.log(generatePassword());
-
+//links the html generate class to a javascript variable
 var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", function genPassElements() {
+//generate button listens for a click to run the generatePassword function
+generateBtn.addEventListener("click", function generatePassword() {
 
-//prompts user to choose a password length that is between 8 and 128 and not a letter
+  //establishes key variables
   var passLength = prompt("plz choose a length between 8 and 128");
   var passLength = Number(passLength);
   var password = '';
-  var characters =[]
+  var characters = []
 
+  //prompts user to choose a password length that is between 8 and 128 and not a letter
   while (passLength < 8 || passLength > 128 || passLength != Number(passLength)) {
     passLength = prompt("plz choose a length between 8 and 128");
   }
-  console.log(passLength);
 
-//prompts user to decide if there will be capital letters in the password and, if so, generates an amount equal to the previously chosen password length
-
+  //prompts user to decide if there will be capital letters in the password and, if so, attaches a random character from the caps variable to the password and pushes it to the characters array
   var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var genCaps = confirm("CAPITAL LETTERS? Y/N");
 
   if (genCaps) {
-      var randomNumber = Math.floor(Math.random() * caps.length);
-      password += caps.substring(randomNumber, randomNumber +1);
-      characters.push(caps);
+    var randomNumber = Math.floor(Math.random() * caps.length);
+    password += caps.substring(randomNumber, randomNumber + 1);
+    characters.push(caps);
   }
-  console.log(allCaps);
 
+  //prompts for lower case letters, pushes a random one to the characters array
   var lows = 'abcdefghijklmnopqrstuvwxyz';
   var genLows = confirm("lowercase letters? y/n");
 
   if (genLows) {
-      var randomNumber = Math.floor(Math.random() * lows.length);
-      password += lows.substring(randomNumber, randomNumber +1);
-      characters.push(lows);
+    var randomNumber = Math.floor(Math.random() * lows.length);
+    password += lows.substring(randomNumber, randomNumber + 1);
+    characters.push(lows);
   }
-  console.log(allLows);
 
-  for (var i = password.length; i < password.length; i ++) {
+  //prompts for special characters, pushes a random one to the characters array
+  var spec = "!@#$%^&*()-+=/.>,<:;|][}{";
+  var genSpec = confirm("$pecial ch@r@cter$? Y/N");
+
+  if (genSpec) {
+    var randomNumber = Math.floor(Math.random() * spec.length);
+    password += spec.substring(randomNumber, randomNumber + 1);
+    characters.push(spec);
+  }
+
+  //prompts for numbers, pushes a random one to the characters array
+  var nums = "0123456789";
+  var genNums = confirm("Numb3rs? Y/N");
+
+  if (genNums) {
+    var randomNumber = Math.floor(Math.random() * nums.length);
+    password += nums.substring(randomNumber, randomNumber + 1);
+    characters.push(nums);
+  }
+
+  //generates the password by taking the previously given password length and pushing random characters from inside the characters array to the password variable
+  for (var i = password.length; i < passLength; i++) {
     var randomNumber = Math.floor(Math.random() * characters.length);
-    var randomLetter = Math.floor(Math.random() * characters[randomNumber].length);
-  }
-
-    var passwordText = document.querySelector("#password");
-  
-      passwordText.value = genPass;
-      console.log(genPass);
+    var randomIndex = Math.floor(Math.random() * characters[randomNumber].length);
+    password += characters[randomNumber].substring(randomIndex, randomIndex + 1);
 
   }
+
+  //writes the password variable to the html variable class
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 );
-
-  // function genSpec() {
-  //   allSpec = '';
-  //   var specs = '!@#$%^&*()-+=/.>,<":;|][}{';
-
-  //   for (var i = 1; i <= passLength; i++) {
-  //     var spec = Math.floor(Math.random() * specs.length + 1);
-
-  //     allSpec += specs.charAt(spec)
-  //   }
-  //   return allSpec;
-  // }
-
-  // function genNums() {
-  //   allNums = '';
-  //   var nums = '0123456789';
-
-  //   for (var i = 1; i <= passLength; i++) {
-  //     var num = Math.floor(Math.random() * nums.length + 1);
-
-  //     allNums += nums.charAt(num)
-  //   }
-  //   return allNums;
-  // }
-  
-
-
-
-
-
-//prompts inside rock paper scissors thing to get a length variable
-//needs to reset to default at start of function
